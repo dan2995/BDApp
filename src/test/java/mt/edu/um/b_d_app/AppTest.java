e593daac1f4519dfb875ccbbeb48da8de082524d
b-azzo <beaazzo@go.net.mt>
2015-04-01 16:28:16 +0200

    Testing Emma

--- src/test/java/mt/edu/um/b_d_app/AppTest.java
+++ src/test/java/mt/edu/um/b_d_app/AppTest.java
@@ -1,5 +1,8 @@
 package mt.edu.um.b_d_app;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -7,9 +10,23 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+    Account a_test;
+    
+    @Before
+    public void setUp()
+    {
+        a_test = new Account();
+    }
+    
     @Test
     public void myTest()
     {
         System.out.println("My test (also checking Jenkins)");
+    }
+    
+    @Test
+    public void AccountNumberTest()
+    {
+        assertEquals("testing_account",a_test.getName());
     }
 }
