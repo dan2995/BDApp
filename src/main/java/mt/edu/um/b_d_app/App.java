606ef1feb86254dcdea8f357b8a939a5fd4f5c14
b-azzo <beaazzo@go.net.mt>
2015-04-01 11:28:36 +0200

    FirstImport

---
+++ src/main/java/mt/edu/um/b_d_app/App.java
@@ -0,0 +1,13 @@
+package mt.edu.um.b_d_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mt/edu/um/b_d_app/AppTest.java
@@ -0,0 +1,15 @@
+package mt.edu.um.b_d_app;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void myTest()
+    {
+        System.out.println("My test");
+    }
+}
