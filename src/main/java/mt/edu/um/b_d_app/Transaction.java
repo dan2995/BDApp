399ae4c7422c806cc133d497544cf71c8b5f3738
b-azzo <beaazzo@go.net.mt>
2015-04-06 17:12:56 +0200

    Transaction Class

--- src/main/java/mt/edu/um/b_d_app/Transaction.java
+++ src/main/java/mt/edu/um/b_d_app/Transaction.java
@@ -11,4 +11,49 @@ package mt.edu.um.b_d_app;
  */
 public class Transaction {
     
+    private int sourceAccountNumber;
+    private int destinationAccountNumber;
+    private long amount;
+    
+    public Transaction()
+    {
+        //handling an invalid transaction by setting all fields to zero
+        //account numbers have been confirmed to be assumed to start from one
+        
+        this.Transaction(0,0,0);
+    }
+    
+    public Transaction(int source, int destination, int amount)
+    {
+        this.sourceAccountNumber = source;
+        this.destinationAccountNumber = destination;
+        this.amount = amount;
+    }
+    
+    public int getSourceAccountNumber()
+    {
+        return this.sourceAccountNumber;
+    }
+    
+    public void setSourceAccountNumber(int accountNumber)
+    {
+        this.sourceAccountNumber = accountNumber;
+    }
+    
+    public int getDestinationAccountNumber()
+    {
+        return this.destinationAccountNumber;
+    }
+    
+    public void setDestinationAccountNumber(int accountNumber)
+    {
+        this.destinationAccountNumber = accountNumber;
+    }
+    
+    //still not sure what it does
+    public boolean process()
+    {
+        return false;
+    }
+    
 }
