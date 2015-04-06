91b97aefcbfbd0573bdc6c0f53acc616dd0c295a
dan2995 <daniela29695@gmail.com>
2015-04-06 16:55:17 +0200

    added addAccount

--- src/main/java/mt/edu/um/b_d_app/AccountDatabase.java
+++ src/main/java/mt/edu/um/b_d_app/AccountDatabase.java
@@ -14,5 +14,16 @@ public class AccountDatabase {
     AccountDatabase(){ //empty constructor - nothing to initialise
     }
 
+    boolean addAccount(int accountNumber, String accountName, long accountBalance){
+        Account account1;
+        for(int i = 0; i < accountVector.size(); i++) {
+            while (accountVector[i].getAccountNumber() != accountNumber){
+                account1 = new Account(accountNumber,accountName,accountBalance);
+                accountVector.add(account1);
+                return true;
+            }
+        }return false;
+    }
+
 
 }
