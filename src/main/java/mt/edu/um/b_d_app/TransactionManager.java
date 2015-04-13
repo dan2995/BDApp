9453e5bb1f5306b5d2ea71147a5b980e0c5ae365
b-azzo <beaazzo@go.net.mt>
2015-04-13 16:31:21 +0200

    Attempt to pass transaction failure test

--- src/main/java/mt/edu/um/b_d_app/TransactionManager.java
+++ src/main/java/mt/edu/um/b_d_app/TransactionManager.java
@@ -14,16 +14,6 @@ public class TransactionManager {
     
     private int numTransactionsProcessed;
     AccountDatabase database;
-    
-    //Proposed handling of the 15 second rule
-    /*
-    Have an array list that stores instances of the class LastUsed, which has the account number and the time of last use 
-    When handling a transaction, traverse the array list. If the current time is more than 15 seconds later, remove the object from the list
-    If the required account numbers have no corresponding object or have been found and deleted, proceed with the transaction and create
-    new objects after the balances have been altered
-    Else return false
-    */
-    
     private ArrayList<LastUsed> active = new ArrayList<LastUsed> ();
     
     public TransactionManager(AccountDatabase database)
@@ -92,14 +82,15 @@ public class TransactionManager {
         //if neither exists
         int i = 0;
         boolean stop = false;
-        while(i<active.size() && (!stop))
+        long now = System.nanoTime();
+        while((i<active.size()) && (!stop))
         {
-            int currentElement = active.get(i).getAccountNumber();
-            if(currentElement == src || currentElement == dst)
+            int currentElement = (active.get(i)).getAccountNumber();
+            if((currentElement == src) || (currentElement == dst))
             {
-                long timestamp = active.get(i).getLastUsed();
-                long now = System.nanoTime();
-                if(((timestamp/1000)+15000000)>=now)//15 seconds has not passed yet (*10^6 to convert to micro seconds)
+                long timestamp = (active.get(i)).getLastUsed();    
+                long passPoint = (timestamp/1000) + 15000000;
+                if(passPoint > now)//15 seconds has not passed yet (*10^6 to convert to micro seconds)
                 {
                     stop = true;
                 }
