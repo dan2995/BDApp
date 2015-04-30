47133fbd10b286627b7c62ba257c4196d971ee64
dan2995 <daniela29695@gmail.com>
2015-04-30 19:36:32 +0200

    removed commenting

--- src/main/java/mt/edu/um/b_d_app/Transaction.java
+++ src/main/java/mt/edu/um/b_d_app/Transaction.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mt.edu.um.b_d_app;
 
-/**
- *
- * @author Beatrix
- */
 public abstract class Transaction {
     
     public abstract boolean process();
