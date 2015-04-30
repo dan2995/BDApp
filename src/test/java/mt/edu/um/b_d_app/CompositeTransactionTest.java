78d0e6efed05f7e51fc317ffcb83f4dcd6e87d17
dan2995 <daniela29695@gmail.com>
2015-04-30 21:18:51 +0200

    started testing

--- src/test/java/mt/edu/um/b_d_app/CompositeTransactionTest.java
+++ src/test/java/mt/edu/um/b_d_app/CompositeTransactionTest.java
@@ -12,7 +12,33 @@ package mt.edu.um.b_d_app;
  * @author Beatrix
  */
 public class CompositeTransactionTest {
-    
+
+    AccountDatabase database1;
+    Account account1;
+    Account account2;
+    Account account3;
+
+
+    @Before
+    public void SetUp() {
+        database1 = new AccountDatabase();
+        account1 = new Account(1, "myAccount1", 100);
+        account2 = new Account(2, "myAccount2", 200);
+        account3 = new Account(3, "myAccount3", 300);
+        TransactionManager(database1);
+        CompositeTransaction("TransactionTest");
+        CompositeTransaction();
+    }
+
+    @Test
+    public void compositeTransactionTest1(){
+        assertEquals(true,account.adjustBalance(30));
+    }
+
+
+
+
+
     //begin
     /*Create
     new accounts (three)
