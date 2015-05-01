25d1c91ff14f388f81443d98971a87c15450ecef
dan2995 <daniela29695@gmail.com>
2015-05-01 16:56:12 +0200

    added creator

--- src/main/java/mt/edu/um/b_d_app/TransactionCreator.java
+++ src/main/java/mt/edu/um/b_d_app/TransactionCreator.java
@@ -1,7 +1,21 @@
 package mt.edu.um.b_d_app;
+import java.util.ArrayList;
 
 /**
  * Created by Daniela on 01/05/2015.
  */
 public class TransactionCreator {
+
+    //transaction manager will receive list of account numbers and find the accounts
+    //move database back to transaction manager
+    public Transaction createTransaction(RiskTypes type, ArrayList<Account> destinationList, ArrayList<Integer> amountList){
+        if(type == RiskTypes.HIGH){
+
+        }
+
+        if(type == RiskTypes.LOW){
+
+        }
+
+    }
 }
