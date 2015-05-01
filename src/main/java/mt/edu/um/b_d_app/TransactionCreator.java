c357e225a6efda5ff3a49d44a79cd8ec4e63d03d
b-azzo <beaazzo@go.net.mt>
2015-05-01 17:05:10 +0200

    Finished TransactionCreator

--- src/main/java/mt/edu/um/b_d_app/TransactionCreator.java
+++ src/main/java/mt/edu/um/b_d_app/TransactionCreator.java
@@ -9,13 +9,36 @@ public class TransactionCreator {
     //transaction manager will receive list of account numbers and find the accounts
     //move database back to transaction manager
     public Transaction createTransaction(RiskTypes type, ArrayList<Account> destinationList, ArrayList<Integer> amountList){
+        
+        TransactionCreator creator = findCreatorForTypeOfTransaction(type);
+        
+        if(creator==null)//return a null transaction of no creator for the type was found
+        {
+            return null;
+        }
+        
+        //Assuming a correct creator was found
+        return creator.createTransaction();
+        
+    }
+    
+    public Transaction createTransaction()
+    {
+        return null;//set to null as no default type was specified in the assignment specification
+    }
+    
+    private TransactionCreator findCreatorForTypeOfTransaction (RiskTypes type)
+    {
         if(type == RiskTypes.HIGH){
-
+            
+            return new HighRiskTransactionCreator();
         }
 
         if(type == RiskTypes.LOW){
-
+            
+            return new LowRiskTransactionCreator();
         }
-
+        
+        return null;
     }
 }
