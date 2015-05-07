f8923d1d64df2c47f30880dff2ea717d3dc65df1
dan2995 <daniela29695@gmail.com>
2015-05-07 14:37:22 +0200

    added transaction creator tests

---
+++ src/test/java/mt/edu/um/b_d_app/TransactionCreatorTest.java
@@ -0,0 +1,35 @@
+package mt.edu.um.b_d_app;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Daniela on 07/05/2015.
+ */
+public class TransactionCreatorTest {
+
+    @Before
+    public void SetUp(){
+
+    }
+
+    @Test
+    public void createTransactionHighTest(){
+
+    }
+
+    @Test
+    public void createTransactionLowTest(){
+
+    }
+
+    @Test
+    public void createTransactionTest(){
+
+    }
+
+    @Test
+    public void createTransactionNoTypeTest(){
+        
+    }
+}
