b16033bca0a2bd974d923bf49b43d66a69646d15
b-azzo <beaazzo@go.net.mt>
2015-05-07 16:22:17 +0200

    skeleton of test class for traversal, sorting, and filtering

---
+++ src/test/java/mt/edu/um/b_d_app/TraversalSortingTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mt.edu.um.b_d_app;
+
+/**
+ *
+ * @author Beatrix
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TraversalSortingTest {
+    
+    @Before
+    public void Setup()
+    {
+        
+    }
+    
+    @Test
+    public void amountAscendingTest()
+    {
+        
+    }
+    
+    @Test
+    public void amountDescendingTest()
+    {
+        
+    }
+           
+    @Test
+    public void srcAccountExistsFilteringTest()
+    {
+        
+    }
+    
+    @Test
+    public void srcAccountDoesNotExistFilteringTest()
+    {
+        
+    }
+    
+}
