ab08576af6f4f9c01b6c7ae13d1fd1ab4f7c433e
b-azzo <beaazzo@go.net.mt>
2015-05-07 14:37:30 +0200

    miscellaneous

---
+++ src/test/java/mt/edu/um/b_d_app/TransactionManagerRiskTypesTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*package mt.edu.um.b_d_app;
+
+/**
+ *
+ * @author Beatrix
+ */
+
+/*import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TransactionManagerRiskTypesTest {
+    
+    @Before
+    public void Setup()
+    {
+        
+    }
+    
+    @Test
+    public void processTransactionTest()
+    {
+        
+    }
+    
+    public boolean processTransaction (RiskTypes type,Account depositDSTAccount, float depositAmount ,ArrayList<Account> destinationList, ArrayList<Double> amountList, AccountDatabase database) throws TransactionFailureException, InterruptedException
+    {
+        TransactionCreator creator = new TransactionCreator();
+        Transaction transaction = creator.createTransaction(type, depositDSTAccount, depositAmount, destinationList, amountList, database);
+        return this.processTransaction(transaction);
+    }
+    
+}*/
