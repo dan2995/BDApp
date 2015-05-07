729c8d34585c9f8feff7e3c94bee1de95853e47e
b-azzo <beaazzo@go.net.mt>
2015-05-07 16:47:40 +0200

    tests continued

--- src/test/java/mt/edu/um/b_d_app/TraversalSortingTest.java
+++ src/test/java/mt/edu/um/b_d_app/TraversalSortingTest.java
@@ -10,6 +10,7 @@ package mt.edu.um.b_d_app;
  * @author Beatrix
  */
 
+import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,12 @@ import org.junit.Test;
 //all of the tests have a return type of atomic accounts
 
 public class TraversalSortingTest {
+    
+    Transaction transaction;
+    AccountDatabase database;
+    ArrayList<AtomicTransaction> transactionsForAcc; 
+    ArrayList<AtomicTransaction> transactionsDescendingAmount;
+    ArrayList<AtomicTransaction> transactionsAscendingAmount;
     
     @Before
     public void Setup()
@@ -27,19 +34,35 @@ public class TraversalSortingTest {
     @Test
     public void amountAscendingTest()
     {
+        ArrayList<AtomicTransaction> result = transaction.getTransactionsAscendingAmount();
         
+        for(int i = 0;i<transactionsAscendingAmount.size();i++)
+        {
+            assertEquals(transactionsAscendingAmount.get(i), result.get(i)); 
+        }
     }
     
     @Test
     public void amountDescendingTest()
     {
+        ArrayList<AtomicTransaction> result = transaction.getTransactionsDescendingAmount();
+        
+        for(int i = 0;i<transactionsDescendingAmount.size();i++)
+        {
+            assertEquals(transactionsDescendingAmount.get(i), result.get(i)); 
+        }
         
     }
            
     @Test
     public void srcAccountExistsFilteringTest()
     {
-        
+        ArrayList<AtomicTransaction> result = transaction.getTransactionsUsingSource(int src);
+       
+        for(int i = 0;i<transactionsForAcc.size();i++)
+        {
+            assertEquals(transactionsForAcc.get(i), result.get(i)); 
+        }
     }
     
     @Test
