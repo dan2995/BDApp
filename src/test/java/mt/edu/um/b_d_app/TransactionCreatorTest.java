4faf7f475a3438c2c74035d11d1b46bcdabcece5
b-azzo <beaazzo@go.net.mt>
2015-05-07 14:42:31 +0200

    first TransactionCreatorTest

--- src/test/java/mt/edu/um/b_d_app/TransactionCreatorTest.java
+++ src/test/java/mt/edu/um/b_d_app/TransactionCreatorTest.java
@@ -1,16 +1,30 @@
 package mt.edu.um.b_d_app;
 
+import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Daniela on 07/05/2015.
  */
 public class TransactionCreatorTest {
 
+    RiskTypes typeL = RiskTypes.LOW;
+    RiskTypes typeH = RiskTypes.HIGH;
+    Account depositDSTAccount;
+    float depositAmount;
+    ArrayList<Account> destinationList;
+    ArrayList<Double> amountList;
+    AccountDatabase database;
+    
+    TransactionCreator creator;
+    
     @Before
     public void SetUp(){
-
+        
+        creator = new TransactionCreator();
+        
     }
 
     @Test
@@ -25,7 +39,7 @@ public class TransactionCreatorTest {
 
     @Test
     public void createTransactionTest(){
-
+        assertEquals(null,creator.createTransaction());
     }
 
     @Test
