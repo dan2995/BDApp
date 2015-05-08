65995baa520e7ee81f53e8a4c371fd91340d481a
dan2995 <daniela29695@gmail.com>
2015-05-08 15:00:30 +0200

    added methods to iterator

--- src/main/java/mt/edu/um/b_d_app/Iterator.java
+++ src/main/java/mt/edu/um/b_d_app/Iterator.java
@@ -1,7 +1,18 @@
 package mt.edu.um.b_d_app;
 
+import com.sun.prism.shader.Solid_TextureFirstPassLCD_Loader;
+
 /**
  * Created by Daniela on 08/05/2015.
  */
-public class Iterator {
+public abstract class Iterator {
+
+    public abstract Transaction first();
+
+    public abstract Transaction next();
+
+    public abstract boolean isDone();
+
+    public abstract Transaction currentItem();
+
 }
