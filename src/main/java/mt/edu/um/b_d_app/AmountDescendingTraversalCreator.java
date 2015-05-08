269631430db6e11679e12e10507c7d26cd33dc5e
dan2995 <daniela29695@gmail.com>
2015-05-08 18:52:51 +0200

    added methods for getTransactionBy()

--- src/main/java/mt/edu/um/b_d_app/AmountDescendingTraversalCreator.java
+++ src/main/java/mt/edu/um/b_d_app/AmountDescendingTraversalCreator.java
@@ -4,4 +4,8 @@ package mt.edu.um.b_d_app;
  * Created by Daniela on 08/05/2015.
  */
 public class AmountDescendingTraversalCreator extends TraversalCreator {
+
+    public ArrayList<AtomicTransaction> getTransactionBy(){
+
+    }
 }
