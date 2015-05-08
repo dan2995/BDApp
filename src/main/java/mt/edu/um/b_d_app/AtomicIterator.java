46dae7dfbc7d7b6ef9107e237892e2e63f6f17b6
b-azzo <beaazzo@go.net.mt>
2015-05-08 16:59:36 +0200

    AtomicIterator

--- src/main/java/mt/edu/um/b_d_app/AtomicIterator.java
+++ src/main/java/mt/edu/um/b_d_app/AtomicIterator.java
@@ -4,12 +4,46 @@ package mt.edu.um.b_d_app;
  * Created by Daniela on 08/05/2015.
  */
 public class AtomicIterator extends Iterator {
+    
+    int index;
+    AtomicTransaction transaction;
+    
+    AtomicIterator(AtomicTransaction transaction)
+    {
+        this.transaction = transaction;
+        this.index = 0;
+    }
+    
+    @Override
+    public Transaction first()
+    {
+            return this.transaction;
+    }        
 
-    public Transaction first();
+    @Override
+    public Transaction next()
+    {
+        if(index==0)
+        {
+            index++;
+            return this.transaction;
+        }
+        return null;
+    }
 
-    public Transaction next();
+    @Override
+    public boolean isDone()
+    {
+        if(index>0)
+        {
+            return true;
+        }
+        return false;
+    }
 
-    public boolean isDone();
-
-    public Transaction currentItem();
+    @Override
+    public Transaction currentItem()
+    {
+        
+    }
 }
