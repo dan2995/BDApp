4bed75e404d2925f071b527dc3d6344a1964696e
b-azzo <beaazzo@go.net.mt>
2015-05-08 17:17:05 +0200

    started iterator

--- src/main/java/mt/edu/um/b_d_app/CompositeIterator.java
+++ src/main/java/mt/edu/um/b_d_app/CompositeIterator.java
@@ -4,8 +4,31 @@ package mt.edu.um.b_d_app;
  * Created by Daniela on 08/05/2015.
  */
 public class CompositeIterator extends Iterator{
+    
+    int index;
+    ArrayList<AtomicTransaction> transactions;
+    
+    CompositeIterator(CompositeTransaction transaction)
+    {
+        if(transaction.getListSize()==0)
+        {
+            transactions = null;
+        }
+        else
+        {
+            for(int i = 0; i<transaction.getListSize();i++)
+            {
+                Transaction current = transaction.getTransaction(i);
+                
+            }
+        }
+        this.index = 0;
+    }
 
-    public Transaction first();
+    public Transaction first()
+    {
+        
+    }
 
     public Transaction next();
 
