8dabf78ed13f6e5f92f9018747c127803e702a0d
dan2995 <daniela29695@gmail.com>
2015-05-08 16:39:44 +0200

    added methods to atomic and composite iterator

--- src/main/java/mt/edu/um/b_d_app/AtomicIterator.java
+++ src/main/java/mt/edu/um/b_d_app/AtomicIterator.java
@@ -3,5 +3,13 @@ package mt.edu.um.b_d_app;
 /**
  * Created by Daniela on 08/05/2015.
  */
-public class AtomicIterator {
+public class AtomicIterator extends Iterator {
+
+    public Transaction first();
+
+    public Transaction next();
+
+    public boolean isDone();
+
+    public Transaction currentItem();
 }
